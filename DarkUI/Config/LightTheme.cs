d3652168353b3899e8d215fe086740ea61b31d24
TFive <t5_@outlook.co.th>
2021-09-18 16:37:53 +0700

    Update LightTheme.cs

--- DarkUI/Config/LightTheme.cs
+++ DarkUI/Config/LightTheme.cs
@@ -10,20 +10,20 @@ namespace DarkUI.Config
 
         public LightTheme()
         {
-            Colors.GreyBackground = Color.FromArgb(180, 183, 185);
-            Colors.HeaderBackground = Color.FromArgb(177, 180, 182);
-            Colors.BlueBackground = Color.FromArgb(186, 197, 215);
-            Colors.DarkBlueBackground = Color.FromArgb(172, 177, 186);
-            Colors.DarkBackground = Color.FromArgb(160, 160, 160);
-            Colors.MediumBackground = Color.FromArgb(169, 171, 173);
-            Colors.LightBackground = Color.FromArgb(189, 193, 194);
-            Colors.LighterBackground = Color.FromArgb(100, 101, 102);
-            Colors.LightestBackground = Color.FromArgb(178, 178, 178);
+            Colors.GreyBackground = Color.FromArgb(220, 223, 225); //Color.FromArgb(180, 183, 185);
+            Colors.HeaderBackground = Color.FromArgb(210, 213, 215); //Color.FromArgb(177, 180, 182);
+            Colors.BlueBackground = Color.FromArgb(200, 203, 205); //Color.FromArgb(186, 197, 215);
+            Colors.DarkBlueBackground = Color.FromArgb(190, 193, 195); //Color.FromArgb(172, 177, 186);
+            Colors.DarkBackground = Color.FromArgb(225, 228, 231); //Color.FromArgb(160, 160, 160);
+            Colors.MediumBackground = Color.FromArgb(215, 218, 221); //Color.FromArgb(169, 171, 173);
+            Colors.LightBackground = Color.FromArgb(192, 193, 194); //Color.FromArgb(189, 193, 194);
+            Colors.LighterBackground = Color.FromArgb(182, 183, 184); //Color.FromArgb(100, 101, 102);
+            Colors.LightestBackground = Color.FromArgb(128, 128, 128); //Color.FromArgb(178, 178, 178);
             Colors.LightBorder = Color.FromArgb(201, 201, 201);
             Colors.DarkBorder = Color.FromArgb(171, 171, 171);
-            Colors.LightText = Color.FromArgb(20, 20, 20);
-            Colors.DisabledText = Color.FromArgb(103, 103, 103);
-            Colors.BlueHighlight = Color.FromArgb(104, 151, 187);
+            Colors.LightText = Color.FromArgb(50, 50, 50); //Color.FromArgb(20, 20, 20);
+            Colors.DisabledText = Color.FromArgb(113, 113, 113); //Color.FromArgb(103, 103, 103);
+            Colors.BlueHighlight = Color.DodgerBlue; //Color.FromArgb(104, 151, 187);
             Colors.BlueSelection = Color.FromArgb(75, 110, 175);
             Colors.GreyHighlight = Color.FromArgb(182, 188, 182);
             Colors.GreySelection = Color.FromArgb(160, 160, 160);
